21c8ed11a8978ad82597ba4eddcbdac0434b43b7
penguinpower <crice@mymail.mines.edu>
2013-09-05 19:05:18 -0600

    .

--- Code/GridSquare.h
+++ Code/GridSquare.h
@@ -2,11 +2,15 @@
 
 class GridSquare {
 	private int x,y;
-	private short type;
+	private short type; // 0 = blank, 1 = light, 2 = mirror
+
 	private const static int WIDTH = 40;
 	private const static int HEIGHT = 30;
 
 	public GridSquare(int x, int y, short type) { this.x = x, this.y =y, this.type = type;}
+
+	public int getX() { return x;}
+	public int getY() { return y;}
 
 	public void setType(short type){ this.type = type;}
 	public short getType() {return type;}
---
+++ Code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, welcome to Light Game\n";
+	return 0;
+}
